8598c5ec4d6ae59d7ec65782f1e8376263c27b41
asinha94 <asinha94@gmail.com>
2017-09-27 17:27:19 -0700

    Modified changes to functional test

--- functional_tests/ros_nodes/camera_two/camera_two.py
+++
@@ -1,6 +0,0 @@
-class camera_two:
-    def __init__(self):
-        pass
-
-    def start(self):
-        pass
--- functional_tests/ros_nodes/hydrophones/hydrophones.py
+++ functional_tests/ros_nodes/hydrophones/hydrophones.py
@@ -2,5 +2,10 @@ class hydrophones:
     def __init__(self):
         pass
 
+    def getParameters(self):
+        return {
+            'port': '/some/port'
+        }
+        
     def start(self):
         pass
